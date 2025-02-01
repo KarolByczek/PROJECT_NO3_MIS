8d0074e3520d7521f683469e3a2e5b07595dbdf8
KarolByczek <byczek_1986@interia.pl>
2025-02-01 11:05:21 +0100

    Manipulation of menu item background color on clicking.

--- MIS/src/components/Menu.tsx
+++ MIS/src/components/Menu.tsx
@@ -1,29 +1,33 @@
 import { Link } from 'react-router-dom';
 
+const onClickHandler = (e:any) => {
+    e.target.style.background = "red";
+}
+
 const Menu = () => {
     return (
         <div className='main_menu'>
             <ul>
                 <Link className="link" to="/guitars">
-                    <div className="menu_link">GUITARS</div>
+                    <div className="menu_link" onClick={onClickHandler}>GUITARS</div>
                 </Link>
                 <Link className="link" to="/saxophones">
-                    <div className="menu_link">SAXOPHONES</div>
+                    <div className="menu_link" onClick={onClickHandler}>SAXOPHONES</div>
                 </Link>
                 <Link className="link" to="/drums">
-                    <div className="menu_link">DRUMS</div>
+                    <div className="menu_link" onClick={onClickHandler}>DRUMS</div>
                 </Link>
                 <Link className="link" to="/keyboards">
-                    <div className="menu_link">KEYBOARDS</div>
+                    <div className="menu_link" onClick={onClickHandler}>KEYBOARDS</div>
                 </Link>
                 <Link className="link" to="/microphones">
-                    <div className="menu_link">MICROPHONES</div>
+                    <div className="menu_link" onClick={onClickHandler}>MICROPHONES</div>
                 </Link>
                 <Link className="link" to="/traditional_instruments">
-                    <div className="menu_link">TRADITIONAL INSTRUMENTS</div>
+                    <div className="menu_link" onClick={onClickHandler}>TRADITIONAL INSTRUMENTS</div>
                 </Link>
                 <Link className="link" to="/accessories">
-                    <div className="menu_link">ACCESSORIES</div>
+                    <div className="menu_link" onClick={onClickHandler}>ACCESSORIES</div>
                 </Link>
             </ul>
         </div>
