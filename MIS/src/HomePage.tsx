6c10e88d83c5530d922240f2ffc4ce6adc45fc1c
KarolByczek <byczek_1986@interia.pl>
2025-02-02 14:38:40 +0100

    Adjustments in the grid menu section.

--- MIS/src/HomePage.tsx
+++ MIS/src/HomePage.tsx
@@ -5,16 +5,16 @@ import ProductStrip from './components/ProductStrip';
 import Carousel from "./components/Carousel";
 
 
-const instrumentIcons:any = [
-  {name: "Recording Equipment", imgURL: "/instrument_icons/instrument-icon (1).png" },
-  {name: "DJ Equipment", imgURL: "/instrument_icons/instrument-icon (2).png" },
-  {name: "Stage Lighting", imgURL: "/instrument_icons/instrument-icon (3).png" },
-  {name: "Speakers", imgURL: "/instrument_icons/instrument-icon (4).png" },
-  {name: "Software", imgURL: "/instrument_icons/instrument-icon (5).png" },
-  {name: "Synthesizers", imgURL: "/instrument_icons/instrument-icon (6).png" },
-  {name: "Pianos", imgURL: "/instrument_icons/instrument-icon (7).png" },
-  {name: "Guitars", imgURL: "/instrument_icons/instrument-icon (8).png" },
-  {name: "Drums", imgURL: "/instrument_icons/instrument-icon (9).png" },
+const instrumentIcons: any = [
+  { name: "Recording Equipment", imgURL: "/instrument_icons/instrument-icon (1).png" },
+  { name: "DJ Equipment", imgURL: "/instrument_icons/instrument-icon (2).png" },
+  { name: "Stage Lighting", imgURL: "/instrument_icons/instrument-icon (3).png" },
+  { name: "Speakers", imgURL: "/instrument_icons/instrument-icon (4).png" },
+  { name: "Software", imgURL: "/instrument_icons/instrument-icon (5).png" },
+  { name: "Synthesizers", imgURL: "/instrument_icons/instrument-icon (6).png" },
+  { name: "Pianos", imgURL: "/instrument_icons/instrument-icon (7).png" },
+  { name: "Guitars", imgURL: "/instrument_icons/instrument-icon (8).png" },
+  { name: "Drums", imgURL: "/instrument_icons/instrument-icon (9).png" },
 ]
 
 function HomePage() {
@@ -29,17 +29,22 @@ function HomePage() {
         <Menu />
         <h1>WELCOME TO THE MUSIC INSTRUMENT SHOP <span>&#128075;</span></h1>
         <Carousel />
-        <div className='grid_menu'>
-          {instrumentIcons.map((gridmenuitem:any, index:number) => {
-            return (
-            <div key={index} className='grid_menu_item'>
-              <img src={gridmenuitem.imgURL} alt="evevi" />
-              <p>
-                {gridmenuitem.name}
-              </p>
-            </div>
-          )})}
+        <div className='grid_menu_section'>
+          <h1>CATEGORIES</h1>
+          <div className='grid_menu'>
+            {instrumentIcons.map((gridmenuitem: any, index: number) => {
+              return (
+                <div key={index} className='grid_menu_item'>
+                  <img src={gridmenuitem.imgURL} alt="evevi" />
+                  <p>
+                    {gridmenuitem.name}
+                  </p>
+                </div>
+              )
+            })}
+          </div>
         </div>
+
       </section>
     </>
   )
