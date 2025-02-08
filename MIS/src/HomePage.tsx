910d2da6d97f785b467c6d64e529f0810bff8363
KarolByczek <byczek_1986@interia.pl>
2025-02-08 16:10:34 +0100

    ...

--- MIS/src/HomePage.tsx
+++ MIS/src/HomePage.tsx
@@ -3,18 +3,19 @@ import HeadStrip from './components/HeadStrip';
 import Menu from './components/Menu';
 import ProductStrip from './components/ProductStrip';
 import Carousel from "./components/Carousel";
+import "./HomePage.scss"
 
 
 const instrumentIcons: any = [
-  { name: "Recording Equipment", imgURL: "/instrument_icons/instrument-icon (1).png" },
-  { name: "DJ Equipment", imgURL: "/instrument_icons/instrument-icon (2).png" },
-  { name: "Stage Lighting", imgURL: "/instrument_icons/instrument-icon (3).png" },
-  { name: "Speakers", imgURL: "/instrument_icons/instrument-icon (4).png" },
-  { name: "Software", imgURL: "/instrument_icons/instrument-icon (5).png" },
-  { name: "Synthesizers", imgURL: "/instrument_icons/instrument-icon (6).png" },
-  { name: "Pianos", imgURL: "/instrument_icons/instrument-icon (7).png" },
-  { name: "Guitars", imgURL: "/instrument_icons/instrument-icon (8).png" },
-  { name: "Drums", imgURL: "/instrument_icons/instrument-icon (9).png" },
+  { name: "Recording Equipment", imgURL: "/instrument_icons/instrument-icon (1).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "DJ Equipment", imgURL: "/instrument_icons/instrument-icon (2).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Stage Lighting", imgURL: "/instrument_icons/instrument-icon (3).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Speakers", imgURL: "/instrument_icons/instrument-icon (4).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Software", imgURL: "/instrument_icons/instrument-icon (5).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Synthesizers", imgURL: "/instrument_icons/instrument-icon (6).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Pianos", imgURL: "/instrument_icons/instrument-icon (7).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Guitars", imgURL: "/instrument_icons/instrument-icon (8).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
+  { name: "Drums", imgURL: "/instrument_icons/instrument-icon (9).png", href: "https://www.youtube.com/watch?v=FRqLYCig6wI" },
 ]
 
 function HomePage() {
@@ -34,12 +35,14 @@ function HomePage() {
           <div className='grid_menu'>
             {instrumentIcons.map((gridmenuitem: any, index: number) => {
               return (
-                <div key={index} className='grid_menu_item'>
-                  <img src={gridmenuitem.imgURL} alt="evevi" />
-                  <p>
-                    {gridmenuitem.name}
-                  </p>
-                </div>
+                <a key={index} className='grid_menu_item' href={gridmenuitem.href}>
+                  <div>
+                    <img src={gridmenuitem.imgURL} alt="evevi" />
+                    <p>
+                      {gridmenuitem.name}
+                    </p>
+                  </div>
+                </a>
               )
             })}
           </div>
